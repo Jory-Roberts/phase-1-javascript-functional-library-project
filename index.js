f39093fa6a9423ae4172c4731c94c7230699d19b
Jory Roberts <robertsjory13@gmail.com>
2023-04-25 03:14:22 -0500

    adds myMap refactor myEach

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function myEach(collection, callback) {
-    let newCollection = [...collection];
+    let newCollection = Array.isArray(collection)
+        ? collection
+        : Object.values(collection);
 
-    if (collection instanceof Array === false) {
-        newCollection = Object.values(collection);
-    }
     for (let i = 0; i < newCollection.length; i++) {
         callback(newCollection[i]);
     }
+
     return collection;
+}
+
+function myMap(collection, callback) {
+    let newCollection = Array.isArray(collection)
+        ? collection
+        : Object.values(collection);
+
+    const newArray = [];
+
+    for (let i = 0; i < newCollection.length; i++) {
+        const newItem = callback(newCollection[i]);
+        newArray.push(newItem);
+    }
+    return newArray;
 }
