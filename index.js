25046ac94890eddadb98acede2f9b2b42a4e5fc0
Jory Roberts <robertsjory13@gmail.com>
2023-04-25 19:26:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -23,3 +23,92 @@ function myMap(collection, callback) {
     }
     return newArray;
 }
+
+//accumulator is optional in myReduce
+function myReduce(collection, callback, acc) {
+    let newCollection = Array.isArray(collection)
+        ? collection
+        : Object.values(collection);
+
+    let i = 0;
+
+    if (!acc) {
+        acc = newCollection[0];
+        i = 1;
+    }
+
+    for (; i < newCollection.length; i++) {
+        acc = callback(acc, newCollection[i]);
+    }
+
+    return acc;
+}
+
+function myFind(collection, predicate) {
+    let newCollection = Array.isArray(collection)
+        ? collection
+        : Object.values(collection);
+
+    for (let i = 0; i < newCollection.length; i++) {
+        if (predicate(newCollection[i])) {
+            return newCollection[i];
+        }
+    }
+}
+
+function myFilter(collection, predicate) {
+    let newCollection = Array.isArray(collection)
+        ? collection
+        : Object.values(collection);
+
+    const newArray = [];
+
+    for (let i = 0; i < newCollection.length; i++) {
+        if (predicate(newCollection[i])) {
+            newArray.push(newCollection[i]);
+        }
+    }
+    return newArray;
+}
+
+function mySize(collection) {
+    let newCollection = Array.isArray(collection)
+        ? collection
+        : Object.values(collection);
+
+    return newCollection.length;
+}
+
+function myFirst(array, n) {
+    if (!n) {
+        return array[0];
+    } else {
+        return array.slice(0, n);
+    }
+}
+
+function myLast(array, n) {
+    if (!n) {
+        return array[array.length - 1];
+    } else {
+        return array.slice(-n);
+    }
+}
+
+function myKeys(object) {
+    const newArray = [];
+
+    for (const key in object) {
+        newArray.push(key);
+    }
+    return newArray;
+}
+
+function myValues(object) {
+    const newArray = [];
+
+    for (const key in object) {
+        newArray.push(object[key]);
+    }
+    return newArray;
+}
